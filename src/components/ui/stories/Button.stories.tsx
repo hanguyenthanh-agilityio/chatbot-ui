c25fe79a58691ef81464b17da0a58434e88010d1
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2026-05-19 17:41:39 +0700

    Remove unused code

--- src/components/ui/stories/Button.stories.tsx
+++ src/components/ui/stories/Button.stories.tsx
@@ -10,7 +10,7 @@ const meta: Meta<typeof Button> = {
   argTypes: {
     variant: {
       control: "select",
-      options: ["primary", "secondary", "outline", "ghost", "danger", "switch"],
+      options: ["primary", "secondary", "outline", "ghost", "danger"],
     },
     size: {
       control: "select",
